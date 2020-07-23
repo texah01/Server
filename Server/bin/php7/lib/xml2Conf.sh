4b937fc31b787bdbf1b99fb845e51b0c7745a542
texah01 <nateelovetexan@gmail.com>
2020-07-23 18:00:18 +0700

    test
    test

---
+++ Server/bin/php7/lib/xml2Conf.sh
@@ -0,0 +1,8 @@
+#
+# Configuration file for using the XML library in GNOME applications
+#
+XML2_LIBDIR="-L/var/jenkins_home/workspace/PHP-7.3-Linux-x86_64/bin/php7/lib"
+XML2_LIBS="-lxml2 -L/var/jenkins_home/workspace/PHP-7.3-Linux-x86_64/bin/php7/lib -lz     -lm "
+XML2_INCLUDEDIR="-I/var/jenkins_home/workspace/PHP-7.3-Linux-x86_64/bin/php7/include/libxml2"
+MODULE_VERSION="xml2-2.9.10"
+
